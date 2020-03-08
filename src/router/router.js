b29e369484474d72d59696f8da2e0d1e57f2e583
mahanmmi <mahanmmi@gmail.com>
2020-03-08 13:33:20 +0330

    Vuex and vue-router added

---
+++ src/router/router.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const routes = [
+
+];
+
+export const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const state = {};
+
+const getters = {};
+
+const mutations = {};
+
+const actions = {};
+
+export const store = new Vuex.Store({
+  state,
+  getters,
+  actions,
+  mutations,
+  modules: []
+});
