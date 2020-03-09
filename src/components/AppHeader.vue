aafe35b77ec0df52a3e50d5ec4cbe1caf70a8a52
mahanmmi <mahanmmi@gmail.com>
2020-03-09 17:12:09 +0330

    header responsive done

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,35 +1,69 @@
 <template>
     <header class="header" :class="{ 'header-shadow': !isFullBlue }">
-        <nav class="main-menu">
-            <router-link
-                to="/"
-                class="main-link link"
-                exact-active-class="link-active"
+        <nav class="main-menu-wrapper">
+            <i
+                v-if="!mobileMainMenu"
+                class="material-icons menu-logo"
+                @click="toggleMobileMainMenu()"
             >
-                صفحه اول
-            </router-link>
-            <router-link
-                to="/graduates"
-                class="main-link link"
-                exact-active-class="link-active"
+                menu
+            </i>
+            <i
+                v-else
+                class="material-icons menu-logo"
+                @click="toggleMobileMainMenu()"
             >
-                فارغ‌التحصیلان
-            </router-link>
-            <router-link
-                to="/voting"
-                class="main-link link"
-                exact-active-class="link-active"
+                close
+            </i>
+            <div
+                class="main-menu"
+                :class="{'main-menu-mobile': mobileMainMenu}"
             >
-                رای‌گیری‌ها
-            </router-link>
+                <router-link
+                    to="/"
+                    class="main-link link"
+                    exact-active-class="link-active"
+                >
+                    صفحه اول
+                </router-link>
+                <router-link
+                    to="/graduates"
+                    class="main-link link"
+                    exact-active-class="link-active"
+                >
+                    فارغ‌التحصیلان
+                </router-link>
+                <router-link
+                    to="/voting"
+                    class="main-link link"
+                    exact-active-class="link-active"
+                >
+                    رای‌گیری‌ها
+                </router-link>
+            </div>
         </nav>
         <div v-if="!isFullBlue" class="logo-container">
             <img alt="logo" style="height: 100%" src="../assets/logo.svg">
         </div>
-        <nav class="user-menu">
-            <div class="user-link link">
-                <p style="display: inline-block; margin-left: 15px">گوگولی</p>
+        <nav class="user-menu-wrapper">
+            <div
+                class="user-logo"
+                :class="{'link-active': userMenu}"
+                @click="toggleUserMenu()"
+            >
+                <p class="user-name">گوگولی</p>
                 <img alt="human" src="../assets/human.svg">
+            </div>
+            <div
+                class="user-menu"
+                :class="{'user-menu-active': userMenu}"
+            >
+                <div class="user-link">
+                    پروفایل من
+                </div>
+                <div class="user-link">
+                    خروج
+                </div>
             </div>
         </nav>
     </header>
@@ -38,6 +72,20 @@
 <script>
   export default {
     name: "AppHeader",
+    data() {
+      return {
+        mobileMainMenu: false,
+        userMenu: false,
+      }
+    },
+    methods: {
+      toggleMobileMainMenu() {
+        this.mobileMainMenu = !this.mobileMainMenu;
+      },
+      toggleUserMenu() {
+        this.userMenu = !this.userMenu;
+      }
+    },
     props: {
       isFullBlue: Boolean
     }
@@ -67,14 +115,52 @@
         box-shadow: 0 0 10px 10px $aut-grad-shadow;
     }
 
-    .main-menu {
+    .main-menu-wrapper {
         display: flex;
         justify-content: flex-start;
+        align-items: center;
         margin-right: 15px;
         flex: 1;
     }
 
-    .link {
+    .main-menu {
+        display: flex;
+    }
+
+    .link, .user-link {
+        text-decoration: none;
+        color: $aut-grad-secondary;
+        padding: 10px 15px;
+        margin: 0 5px;
+        border-radius: 10px;
+        cursor: pointer;
+    }
+
+    .user-link {
+        font-size: large;
+    }
+
+    .link-active, .link:hover, .user-link:hover, .user-logo:hover {
+        background-color: $aut-grad-primary-dark;
+    }
+
+    .logo-container {
+        height: 70px;
+    }
+
+    .user-menu-wrapper {
+        display: flex;
+        flex: 1;
+        margin-left: 15px;
+        align-items: center;
+        flex-direction: row-reverse;
+        justify-content: flex-start;
+    }
+
+    .user-logo {
+        display: flex;
+        align-items: center;
+        cursor: pointer;
         text-decoration: none;
         color: $aut-grad-secondary;
         padding: 10px 15px;
@@ -82,27 +168,65 @@
         border-radius: 10px;
     }
 
-    .link-active, .link:hover {
-        background-color: $aut-grad-primary-dark;
-    }
-
-    .main-link {
-
-    }
-
-    .logo-container {
-        height: 70px;
-    }
-
     .user-menu {
-        display: flex;
-        flex: 1;
-        margin-left: 15px;
-        flex-direction: row-reverse;
-        justify-content: flex-start;
+        display: none;
+        background-color: $aut-grad-primary;
+
+        padding: 5px;
+        flex-direction: column;
+        position: absolute;
+        border-bottom-right-radius: 5px;
+        border-bottom-left-radius: 5px;
+        margin-top: 80px;
     }
 
-    .user-link {
+    .user-menu-active {
         display: flex;
+    }
+
+    .user-name {
+        display: inline-block;
+        margin-left: 15px
+    }
+
+    .menu-logo {
+        cursor: pointer;
+        font-size: xx-large;
+        display: none;
+    }
+
+    @media only screen and (max-width: 768px) {
+        .user-name {
+            display: none;
+        }
+
+        .link, .user-link {
+            font-size: medium;
+        }
+
+        .menu-logo {
+            display: block;
+            position: relative;
+        }
+
+        .main-menu {
+            display: none;
+        }
+
+        .main-menu-mobile {
+            display: flex;
+            background-color: $aut-grad-primary;
+            padding: 5px;
+            flex-direction: column;
+            position: absolute;
+            border-bottom-right-radius: 5px;
+            border-bottom-left-radius: 5px;
+            margin-top: 80px;
+        }
+
+        .user-logo {
+            position: relative;
+        }
+
     }
 </style>
